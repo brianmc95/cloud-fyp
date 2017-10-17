5b75b892d7c37ce7541cf1c4f594b076e19d3c10
Brian McCarthy <brianmccarthy95@gmail.com>
2017-10-17 12:02:28 +0100

    EBS_volume deployment successful

--- aws/EBS_volume.py
+++ aws/EBS_volume.py
@@ -19,15 +19,34 @@ class EBS_volume:
             Determine these
     """
 
-    def __init__(self, size, name, region=None, location=None, snapshot=None):
+    def __init__(self, size, name, region=None, location=None, snapshot=None, accessID_Local=".", accessKey_Local="."):
         self.size = size
         self.name = name
         self.region = region
         self.location = location
         self.snapshot = snapshot
 
-        self._cls = get_driver(Provider.EC2)
-        self._driver = self._cls("To be filled in", "To be filled in", region=self.region)
+        self.__accessID, self.__accessKey = self.__read_in_access_keys(accessID_Local, accessKey_Local)
+
+        self.__cls = get_driver(Provider.EC2)
+        self.__driver = self.__cls("To be filled in", "To be filled in", region=self.region)
+
+        self.attached_nodes = {}
+        self.__volume = None
+
+
+    def __read_in_access_keys(self, accessIDLocal, accessKeyLocal):
+        accessID_file = open(accessIDLocal, "r")
+        accessKey_file = open(accessKeyLocal, "r")
+        accessID = accessID_file.readline()
+        accessKey = accessKey_file.readline()
+        return accessID, accessKey
+
+    def __str__(self):
+        return "< %s , %s , %s >" % (self.size, self.name, self.region)
+
+    def __del__(self):
+        return "Volume %s: destroyed" & (self.name)
 
     def set_size(self, size):
         self.size = size
@@ -54,7 +73,29 @@ class EBS_volume:
         return self.snapshot
 
     def create_volume(self):
-        locations = self._driver.list_locations()
+        locations = self.__driver.list_locations()
         location = [r for r in locations if r.availability_zone.region_name == self.region][0]
-        print(location)
-        volume = self._driver.create_volume(size=8, name="Test GP volume", ex_volume_type="gp2", location=location)
+        volume = self.__driver.create_volume(size=8, name="Test GP volume", ex_volume_type="gp2", location=location)
+        self.__volume = volume
+        print("Volume created")
+
+    def delete_volume(self, volume_name):
+        if volume_name == self.name:
+            self.__driver.destroy_volume(self.__volume)
+            del self
+
+    def create_snapshot(self, volume_name):
+        return self.__driver.create_volume_snapshot(self.__volume, volume_name)
+
+    def attach_volume(self, node, volume_name, device="/dev/sdb"):
+        if volume_name == self.name:
+            success = self.__driver.attach_volume(node, self.__volume)
+            if success:
+                self.attached_nodes[node.get_name()] = node
+
+    def detach_volume(self, node, volume_name):
+        if volume_name == self.name:
+            success = self.__driver.attach_volume(self.__volume)
+            if success:
+                self.attached_nodes.pop(node.get_name())
+
