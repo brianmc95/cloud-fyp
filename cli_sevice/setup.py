ce258e9692b616bdc4956571d8b574a3e980abd5
Brian McCarthy <brianmccarthy95@gmail.com>
2018-04-01 11:22:14 +0100

    Rough draft of cli

---
+++ cli_sevice/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="cloudService",
+    version="1.0",
+    py_modules=["cloudService"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
+        [console_scripts]
+        cloudService=cloudService:cli
+    """,
+)
